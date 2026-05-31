a3bf4588d4213f36084e42744ff82bce8c42bfab
opeCre8tivia <opegude.n3t@gmail.com>
2026-05-31 16:43:56 +0300

    mobile:added app icon and logo

--- tmisgomobile/android/app/src/main/java/com/tmisgo/MainApplication.kt
+++ tmisgomobile/android/app/src/main/java/com/tmisgo/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.saitiyo
+package com.tmisgo
 
 import android.app.Application
 import com.facebook.react.PackageList
--- tmisgomobile/ios/TMISGO/AppDelegate.swift
+++ tmisgomobile/ios/TMISGO/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "saitiyo",
+      withModuleName: "TMISGO",
       in: window,
       launchOptions: launchOptions
     )
